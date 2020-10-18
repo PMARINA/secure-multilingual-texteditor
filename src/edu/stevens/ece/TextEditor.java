75753fd9372e432c9a8f1f102f8e84592baba552
Dov Kruger <Dov.Kruger@stevens.edu>
2020-10-18 10:01:26 -0400

    added saving into text editor

--- src/edu/stevens/ece/TextEditor.java
+++ src/edu/stevens/ece/TextEditor.java
@@ -5,10 +5,21 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.text.*;
 import gutil.*;
+import java.util.zip.*;
+import java.io.*;
 
 public class TextEditor extends App {
 	private JTextPane editor;
 	private DefaultStyledDocument doc;
+	private static JFileChooser fileChooser;
+	private static DefaultEditorKit editorKit;
+	static {
+		editorKit = new DefaultEditorKit();
+		fileChooser = new JFileChooser();
+	}
+	public void statusAlert(Exception e) {
+		e.printStackTrace(); // for now, just dump to console
+	}
 	public TextEditor() throws Exception {
 		super(new Conf("conf/stex.conf")); // TODO: get conf file from home directory
 		setSize(1000,800);
@@ -18,9 +29,24 @@ public class TextEditor extends App {
 		add(editorScrollPane, BorderLayout.CENTER);
 		setVisible(true);
 	}
+	public void addActions() {
+		
+	}
 	public void open() {
 	}
 	public void save() {
+		try {
+			int returnVal = fileChooser.showOpenDialog(this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fileChooser.getSelectedFile();
+				GZIPOutputStream gz = new GZIPOutputStream(new FileOutputStream(file));
+				Writer writer = new BufferedWriter(new OutputStreamWriter(gz));
+				editorKit.write(writer, doc, 0, doc.getLength());
+				writer.close();
+			}
+		} catch(Exception e) { // if anything goes wrong, alert in the status bar
+			statusAlert(e);
+		}
 	}
 	public static void main(String[] args) throws Exception {
 		new TextEditor();
